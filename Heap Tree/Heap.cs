4fab5a1ff98a4307bc36de1a76dccaf66d4cd97c
Nicholas Finefter <nfinefter@gmail.com>
2022-05-17 15:46:43 -0700

    Add project files.

---
+++ Heap Tree/Heap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heap_Tree
+{
+    class Heap<T>
+    {
+        public void Insert(T item, T[] items)
+        {
+            T[] newItems = new T[items.Length + 1];
+            newItems = items;
+            newItems[items.Length] = item;
+        }
+        public void HeapifyUp()
+        {
+
+        }
+        public void HeapifyDown()
+        {
+
+        }
+    }
+}
---
+++ Heap Tree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Heap_Tree
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
