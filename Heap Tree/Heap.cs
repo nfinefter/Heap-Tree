36293cb71e58d8775205c4c1d6a78b5d5b1dceda
Nicholas Finefter <nfinefter@gmail.com>
2022-05-30 14:43:21 -0700

    wrote some functions

--- Heap Tree/Heap.cs
+++ Heap Tree/Heap.cs
@@ -4,25 +4,72 @@ using System.Text;
 
 namespace Heap_Tree
 {
-    class Heap<T>
+    class Heap<T> where T : IComparable<T>
     {
-        public void Insert(T item, T[] items)
+        private T[] items;
+
+        public void Insert(T item)
         {
             T[] newItems = new T[items.Length + 1];
             newItems = items;
             newItems[items.Length] = item;
         }
-        public void HeapifyUp()
+        public void HeapifyUp(int index)
         {
+            if (index == 0) return;
 
+            int parent = (index - 1) / 2;
+
+            if (items[index].CompareTo(items[parent]) < 0)
+            {
+                T temp = items[parent];
+                items[parent] = items[index];
+                items[index] = temp;
+            }
+            HeapifyUp(parent);
         }
-        public void HeapifyDown()
+        public void HeapifyDown(int index)
         {
+            int leftChild = index * 2 + 1;
 
+            if (leftChild >= items.Length)
+            {
+                return;
+            }
+
+            int swapIndex = 0;
+            int rightChild = index * 2 + 2;
+
+            if (rightChild >= items.Length)
+            {
+                swapIndex = leftChild;
+            }
+            else
+            {
+                if (items[leftChild] != null && items[rightChild] != null)
+                {
+                    swapIndex = Math.Min(items[leftChild], items[rightChild]);
+                }
+                
+            }
+            if (items[swapIndex].CompareTo(items[index]) < 0)
+            {
+                T temp = items[index];
+                items[index] = items[swapIndex];
+                items[swapIndex] = temp;
+            }
+            HeapifyDown(swapIndex);
         }
-        public void Pop()
+        public T Pop()
         {
+            if (items.Length == 0) throw new Exception("empty");
 
+            T root = items[0];
+
+            items[0] = items[items.Length - 1];
+
+            HeapifyDown(0);
+            return root;
         }
     }
 }
