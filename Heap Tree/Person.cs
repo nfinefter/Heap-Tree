798ae73e87e63971fa5844ada768629d6765a8e1
Nicholas Finefter <nfinefter@gmail.com>
2022-06-02 19:37:31 -0700

    Made sorting with people

---
+++ Heap Tree/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heap_Tree
+{
+    class Person : IComparable<Person>
+    {
+        public int FastPassLevel;
+        public int LinePos;
+
+        public Person(int fastPassLevel, int linePos)
+        {
+            FastPassLevel = fastPassLevel;
+            LinePos = linePos;
+        }
+
+        public override string ToString()
+        {
+            return $"FastPass Level: {FastPassLevel}; Line Position: {LinePos}";
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (other.FastPassLevel > FastPassLevel)
+            {
+                return -1;
+            }
+            
+            if (other.FastPassLevel < FastPassLevel)
+            {
+                return 1;
+            }
+            
+            if (other.LinePos < LinePos)
+            {
+                return -1;
+            }
+
+            if (other.LinePos > LinePos)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
--- Heap Tree/Program.cs
+++ Heap Tree/Program.cs
@@ -7,14 +7,19 @@ namespace Heap_Tree
         //Max Heap
         static void Main(string[] args)
         {
-            Heap<string> items = new Heap<string>(5);
+            Heap<Person> items = new Heap<Person>(5);
 
-            items.Push("hi");
-            items.Push("abc");
-            items.Push("bye");
-            items.Push("zzzzz");
-            items.Push("aaaaa");
-            items.Push("aaaa");
+            items.Push(new Person(5, 2));
+            items.Push(new Person(2, 1));
+            items.Push(new Person(5, 1));
+            items.Push(new Person(3, 2));
+
+            //items.Push("hi");
+            //items.Push("abc");
+            //items.Push("bye");
+            //items.Push("zzzzz");
+            //items.Push("aaaaa");
+            //items.Push("aaaa");
 
             while (items.Count > 0)
             {
